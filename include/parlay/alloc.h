8c1b39c76d2e434daad91bb34fefd1b2975cf57e
Daniel Anderson <dlanders@cs.cmu.edu>
2020-08-27 16:54:56 -0400

    Fix bug in allocator where final pool size wraps around to negative

--- include/parlay/alloc.h
+++ include/parlay/alloc.h
@@ -193,7 +193,7 @@ inline std::vector<size_t> default_sizes() {
 
   std::vector<size_t> sizes;
   for (size_t i = log_min_size; i <= log_max_size; i++)
-    sizes.push_back(1 << i);
+    sizes.push_back(size_t{1} << i);
   return sizes;
 }
 
