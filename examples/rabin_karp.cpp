21e9cecc81354edb9d1c50d2ac80df7464a4a16b
Guy Blelloch <guyb@cs.cmu.edu>
2022-01-31 15:52:27 -0500

    added rabin_karp example

--- examples/rabin_karp.cpp
+++ examples/rabin_karp.cpp
@@ -1,64 +1,54 @@
 #include <parlay/primitives.h>
 #include <parlay/io.h>
-#include <parlay/internal/block_delayed.h>
-namespace delayed = parlay::block_delayed;
 
-using ulong = unsigned long;
-const ulong p = 1045678717;
-static const ulong x = 500000000;
-auto multm = parlay::make_monoid([] (ulong a, ulong b) {return (a*b)%p;}, 1ul);
-auto addm = parlay::make_monoid([] (ulong a, ulong b) {return (a+b)%p;}, 0ul);
+// **************************************************************
+// Rabin-Karp string matching.
+// Generates a running hash such that the difference been two
+// positions gives a hash for the string in between.  The search string
+// can then be compared with all pairs of positions.
+// **************************************************************
 
-template <typename T>
-struct rabin_karp {
-private:
-  parlay::sequence<ulong> hashes;
-  parlay::sequence<T> str;
-  ulong total;
-  long n;
-
-  auto powers_of_x(long n) {
-    auto xs = parlay::delayed_tabulate(n, [&] (long i) {return x;});
-    auto [powers, total] = parlay::scan(xs, multm);
-    return powers;
-  }
-
-  template <typename Range>
-  auto polynomial_terms(const Range& s) {
-    //return delayed::zip_with(parlay::iota(n), powers_of_x(n),
-    // [&] (long i, ulong xi) {
-    //return ((ulong) s[i]) * xi;});
-    auto foo = powers_of_x(n);
-    return parlay::tabulate(s.size(), [&] (long i) {
-    					return (((ulong) s[i]) * foo[i])%p;});
-  }
-
-public:
-  template <typename Range>
-  rabin_karp(const Range& s)
-    : n(std::size(s)), str(parlay::to_sequence(s)) {
-    auto [c, sum] = parlay::scan(polynomial_terms(s), addm);
-    hashes = c; // delayed::force(c);
-    total = sum;
-  }
-
-  template <typename Range>
-  long find(const Range& s) {
-    long m = s.size();
-    ulong hash = parlay::reduce(polynomial_terms(s), addm);
-    auto check = [&] (long i, ulong xi) {
-		   if (i > n - m) return n;
-		   ulong hash_end = (i == n - m) ? total: hashes[i+m];
-		   if (((hash*xi) + hashes[i])%p == hash_end) return i;
-		   return n;
-		 };
-    auto a = delayed::zip_with(parlay::iota(n), powers_of_x(n), check);
-    return delayed::reduce(a, parlay::minm<long>());
-    //auto cc = powers_of_x(n);
-    //auto a = parlay::tabulate(n, [&] (long i) {return check(i, cc[i]);});
-    //return parlay::reduce(a, parlay::minm<long>());
-  }
+struct field {
+  static constexpr unsigned int p = 1045678717;
+  using l = unsigned long;
+  unsigned int val;
+  template <typename Int>
+  field(Int i) : val(i) {}
+  field() {}
+  field operator+(field a) {return field(((l) val + (l) a.val)%p);}
+  field operator*(field a) {return field(((l) val * (l) a.val)%p);}
+  bool operator==(field a) {return val == a.val;}
 };
+auto multm = parlay::monoid([] (field a, field b) {return a*b;}, 1);
+
+template <typename Range1, typename Range2>
+long rabin_karp(const Range1& s, const Range2& str) {
+  long n = s.size();
+  long m = str.size();
+  field x(500000000);
+
+  // calculate hashes for prefixes of s
+  auto xs = parlay::delayed_tabulate(n, [&] (long i) {
+	      return x;});
+  auto [powers, total] = parlay::scan(xs, multm);
+  auto terms = parlay::tabulate(n, [&] (long i) {
+		 return field(s[i]) * powers[i];});
+  auto [hashes, sum] = parlay::scan(terms);
+  
+  // calculate hash for str
+  auto terms2 = parlay::delayed_tabulate(m, [&] (long i) {
+		  return field(str[i]) * powers[i];});
+  field hash = parlay::reduce(terms2);
+  
+  // find matches
+  auto y = parlay::delayed_tabulate(n-m+1, [&] (long i) {
+	     field hash_end = (i == n - m) ? total: hashes[i+m];
+	     if (hash * powers[i] + hashes[i] == hash_end &&
+		 parlay::equal(str, s.cut(i,i+m)))
+	       return i;
+	     return n; });
+  return parlay::reduce(y, parlay::minm<long>());
+}
 
 // **************************************************************
 // Driver code
@@ -69,10 +59,11 @@ int main(int argc, char* argv[]) {
   auto usage = "Usage: rabin_karp <search_string> <filename>";
   if (argc != 3) std::cout << usage << std::endl;
   else {
-    charseq search_str = parlay::to_chars(argv[1]);
     charseq str = parlay::chars_from_file(argv[2]);
-    rabin_karp<char> rk(str);
-    long loc = rk.find(search_str);
-    std::cout << "location found : " << loc << std::endl;
+    charseq search_str = parlay::to_chars(argv[1]);
+    long loc = rabin_karp(str, search_str);
+    if (loc < str.size())  
+      std::cout << "found at position: " << loc << std::endl;
+    else std::cout << "not found" << std::endl;
   }
 }
