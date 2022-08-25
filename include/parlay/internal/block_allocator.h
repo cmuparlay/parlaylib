e842d63c25c8d25d78ea51182dabf7cb17191910
Daniel Anderson <dlanders@cs.cmu.edu>
2022-08-24 21:34:19 -0400

    Make block allocator conservative again in case of user code that allocates while holding a lock

--- include/parlay/internal/block_allocator.h
+++ include/parlay/internal/block_allocator.h
@@ -85,7 +85,7 @@ struct block_allocator {
   auto initialize_list(std::byte* buffer) const -> block* {
     parallel_for (0, list_length - 1, [&] (size_t i) {
       new (buffer + i * block_size) block{get_block(buffer, i+1)};
-    });
+    }, 0, true);
     new (buffer + (list_length - 1) * block_size) block{nullptr};
     return get_block(buffer, 0);
   }
