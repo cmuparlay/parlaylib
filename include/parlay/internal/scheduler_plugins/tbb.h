9faf766d8fdf7dfc6fe3adcac62a91235990a695
Daniel Anderson <dlanders@cs.cmu.edu>
2023-02-08 13:01:14 -0500

    Update TBB plugin to support latest TBB API

--- include/parlay/internal/scheduler_plugins/tbb.h
+++ include/parlay/internal/scheduler_plugins/tbb.h
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 
+#include <type_traits>
+
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_invoke.h>
@@ -13,33 +15,38 @@ namespace parlay {
 // IWYU pragma: private, include "../../parallel.h"
 
 inline size_t num_workers() { return tbb::this_task_arena::max_concurrency(); }
-inline size_t worker_id() { return tbb::task_arena::current_thread_index(); }
 
-template <class F>
-inline void parallel_for(size_t start, size_t end, F f, long granularity, bool) {
-  if (end > start) {
-    if (granularity == 0) {
-      tbb::parallel_for(tbb::blocked_range<size_t>(start, end), [&](const tbb::blocked_range<size_t>& r) {
-        for (auto i = r.begin(); i != r.end(); ++i) {
-          f(i);
-        }
-      });
-    }
-    else {
-      size_t n_blocks = (end - start + granularity - 1) / granularity;
-      size_t block_size = (end - start + n_blocks - 1) / n_blocks;
-      tbb::parallel_for(size_t{0}, n_blocks, [&](size_t b) {
-        for (size_t i = b * block_size + start; i < (b + 1) * block_size + start && i < end; i++) {
-          f(i);
-        }
-      });
-    }
+inline size_t worker_id() {
+  auto id = tbb::this_task_arena::current_thread_index();
+  return id == tbb::task_arena::not_initialized ? 0 : id;
+}
+
+template <typename F>
+inline void parallel_for(size_t start, size_t end, F&& f, long granularity, bool) {
+  static_assert(std::is_invocable_v<F&, size_t>);
+  // Use TBB's automatic granularity partitioner (tbb::auto_partitioner)
+  if (granularity == 0) {
+    tbb::parallel_for(tbb::blocked_range<size_t>(start, end), [&](const tbb::blocked_range<size_t>& r) {
+      for (auto i = r.begin(); i != r.end(); ++i) {
+        f(i);
+      }
+    }, tbb::auto_partitioner{});
+  }
+  // Otherwise, use the granularity specified by the user (tbb::simple_partitioner)
+  else {
+    tbb::parallel_for(tbb::blocked_range<size_t>(start, end, granularity), [&](const tbb::blocked_range<size_t>& r) {
+      for (auto i = r.begin(); i != r.end(); ++i) {
+        f(i);
+      }
+    }, tbb::simple_partitioner{});
   }
 }
 
 template <typename Lf, typename Rf>
-inline void par_do(Lf left, Rf right, bool) {
-  tbb::parallel_invoke(left, right);
+inline void par_do(Lf&& left, Rf&& right, bool) {
+  static_assert(std::is_invocable_v<Lf&&>);
+  static_assert(std::is_invocable_v<Rf&&>);
+  tbb::parallel_invoke(std::forward<Lf>(left), std::forward<Rf>(right));
 }
 
 }  // namespace parlay
