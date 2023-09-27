f916c1fdbac83edece5ae89bbe8a6fdac96f7cc1
Daniel Anderson <dlanders@cs.cmu.edu>
2023-09-27 17:32:04 -0400

    Change interface of bigatomic and improve CAS [skip ci]

--- include/parlay/internal/concurrency/big_atomic.h
+++ include/parlay/internal/concurrency/big_atomic.h
@@ -43,11 +43,12 @@ T bits_to_object(const char* src) {
 
 
 
-template<typename T>
+template<typename T, typename Equal = std::equal_to<>>
 struct big_atomic {
   // T must be trivially copyable, but it doesn't have to be trivially default constructible (or
   // even default constructible at all) since we only make copies from what the user gives us
   static_assert(std::is_trivially_copyable_v<T>);
+  static_assert(std::is_invocable_r_v<bool, Equal, T&&, T&&>);
 
  private:
   struct indirect_holder {
@@ -71,18 +72,19 @@ struct big_atomic {
 
   using allocator = type_allocator<indirect_holder>;
 
-  big_atomic() : version(0), indirect_value(nullptr), fast_value{} {
+  big_atomic() : version(0), indirect_value(allocator::create(T{})), fast_value{} {
     static_assert(std::is_default_constructible_v<T>);
 
     // force correct static initialization order
     allocator().init();
     hazptr_instance();
 
-    T t{};              // Default construct the initial value
+    T t{};
     std::memcpy(&fast_value, &t, sizeof(T));
   }
 
-  explicit big_atomic(T t) : version(0), indirect_value(nullptr), fast_value{} {
+  /* implicit */ big_atomic(const T& t) : version(0), indirect_value(allocator::create(t)), fast_value{} {  // NOLINT(google-explicit-constructor)
+
     // force correct static initialization order
     allocator().init();
     hazptr_instance();
@@ -119,20 +121,23 @@ struct big_atomic {
     try_seqlock_and_store(num, desired, new_p);
   }
 
-  bool compare_and_swap(const T& expected, const T& desired) {
-    auto reader = read_fast();
+  bool cas(const T& expected, const T& desired) {
+    if (Equal{}(expected, desired)) {
+      return load() == expected;
+    }
 
+    auto num = version.load(std::memory_order_acquire);
+
+    // Can't use a fast read here -- we *need* to take a hazard pointer even if
+    // the fast value is valid because otherwise our CAS below could ABA!!
     auto hazptr = hazptr_holder{};
-    auto p = (reader.fast_valid) ? reader.p : hazptr.protect(indirect_value);
-    T current = (reader.fast_valid) ? reader.get_fast() : p->value;
+    auto p = hazptr.protect(indirect_value);
+    assert(p != nullptr);
 
-    if (!(current == expected)) {
+    if (!Equal{}(p->value, expected)) {
       return false;
     }
-    else if (current == expected && expected == desired) {
-      return true;
-    }
-    
+
     auto new_p = marked_indirect_ptr(allocator::create(desired)).set_mark(SLOW_MODE);
     auto old_p = p;
     
@@ -140,7 +145,7 @@ struct big_atomic {
          || (p == old_p.clear_mark() && indirect_value.compare_exchange_strong(p, new_p))) {
            
       retire(p);
-      try_seqlock_and_store(reader.num, desired, new_p);
+      try_seqlock_and_store(num, desired, new_p);
       return true;
     }
     else {
@@ -172,12 +177,13 @@ struct big_atomic {
   };
 
   fast_reader read_fast() {
-    fast_reader loader;
-    loader.num = version.load(std::memory_order_acquire);
-    internal::atomic_load_per_byte_memcpy(&loader.buffer, &fast_value, sizeof(T));
-    loader.p = indirect_value.load();
-    loader.fast_valid = loader.p.get_mark() != SLOW_MODE && loader.num == version.load(std::memory_order_relaxed);
-    return loader;
+    fast_reader reader;
+    reader.num = version.load(std::memory_order_acquire);
+    internal::atomic_load_per_byte_memcpy(&reader.buffer, &fast_value, sizeof(T));
+    reader.p = indirect_value.load();
+    assert(reader.p != nullptr);
+    reader.fast_valid = reader.p.get_mark() != SLOW_MODE && reader.num == version.load(std::memory_order_relaxed);
+    return reader;
   }
 
   void try_seqlock_and_store(version_type num, const T& desired, marked_indirect_ptr p) noexcept {
--- test/test_big_atomic.cpp
+++ test/test_big_atomic.cpp
@@ -37,7 +37,7 @@ TEST(TestBigAtomic, TestStore) {
 TEST(TestBigAtomic, TestCasSuccess) {
   parlay::big_atomic<ManyLongs> ba({1,2,3});
   auto expected = ba.load();
-  ASSERT_TRUE(ba.compare_and_swap(expected, ManyLongs{4,5,6}));
+  ASSERT_TRUE(ba.cas(expected, ManyLongs{4,5,6}));
   auto val = ba.load();
   ASSERT_EQ(val, ManyLongs({4,5,6}));
 }
@@ -46,7 +46,7 @@ TEST(TestBigAtomic, TestCasFailure) {
   parlay::big_atomic<ManyLongs> ba({1,2,3});
   auto expected = ba.load();
   expected.x = 2;
-  ASSERT_FALSE(ba.compare_and_swap(expected, ManyLongs{4,5,6}));
+  ASSERT_FALSE(ba.cas(expected, ManyLongs{4,5,6}));
   auto val = ba.load();
   ASSERT_EQ(val, ManyLongs({1,2,3}));
 }
@@ -59,7 +59,7 @@ TEST(TestBigAtomic, TestConcurrent) {
     parlay::parallel_for(0, 100000, [&](long long i) {
       auto val = ba.load();
       auto new_val = ManyLongs{i, 2 * i, 3 * i};
-      if (ba.compare_and_swap(val, new_val)) {
+      if (ba.cas(val, new_val)) {
         total1.fetch_add(val.x + val.y + val.z);
         total2.fetch_add(new_val.x + new_val.y + new_val.z);
       }
