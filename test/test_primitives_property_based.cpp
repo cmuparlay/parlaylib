0f353562d4c9b796e0cc37a9d944f98374dc5693
Peter Li <lihaohong6@gmail.com>
2024-12-04 20:42:34 -0500

    Add more property-based tests for primitives

--- test/test_primitives_property_based.cpp
+++ test/test_primitives_property_based.cpp
@@ -22,6 +22,14 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
 }
 
 RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testCopy,
+              (const parlay::sequence<int> &list)) {
+  parlay::sequence<int> list2(list.size());
+  parlay::copy(list, list2);
+  RC_ASSERT(list == list2);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
               testReduce,
               (const std::vector<int64_t> &list)) {
   int64_t reduce = parlay::reduce(list);
@@ -32,6 +40,15 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
 
   int64_t reduce_max = parlay::reduce(list, parlay::maxm<int64_t>());
   RC_ASSERT(reduce_max == *std::max_element(list.begin(), list.end()));
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testReduceBool,
+              (const parlay::sequence<bool> &list)) {
+  parlay::monoid monoid([](int x, int y) {return x+y;}, 0);
+  int actual = parlay::reduce(list, monoid);
+  int expected = parlay::reduce(parlay::map(list, [](bool b) {return (int)b;}));
+  RC_ASSERT(expected == actual);
 }
 
 RC_GTEST_PROP(TestPrimitivePropertyBased,
@@ -61,19 +78,102 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
   RC_ASSERT(scan_result == scan_result2);
 }
 
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testPack,
+              (parlay::sequence<int> list1,
+               parlay::sequence<bool> list2)) {
+  size_t size = std::min(list1.size(), list2.size());
+  // FIXME: test fails/segfaults when list1 and list2 have different sizes
+  auto actual = parlay::pack(list1.cut(0, size), list2.cut(0, size));
+  auto expected = parlay::sequence<int>();
+  for (size_t i = 0; i < size; i++) {
+    if (list2[i]) {
+      expected.push_back(list1[i]);
+    }
+  }
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testPackSameAsFilter,
+              (const parlay::sequence<int> &list, int mod, int threshold)) {
+  RC_PRE(mod != 0);
+  threshold %= mod;
+  auto f = [=](int num) {return num % mod < threshold;};
+  auto expected = parlay::filter(list, f);
+  auto bool_list = parlay::map(list, f);
+  auto actual = parlay::pack(list, bool_list);
+  RC_ASSERT(expected == actual);
+  // Test pack_into_uninitialized
+  int count = parlay::reduce(bool_list, parlay::monoid([](int a, int b){return a + b;}, 0));
+  actual = parlay::sequence<int>::uninitialized(count);
+  parlay::pack_into_uninitialized(list, bool_list, actual);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testPackIndex,
+              (const parlay::sequence<bool> &list)) {
+  auto actual = parlay::pack_index(list);
+  auto expected = parlay::pack(parlay::iota(list.size()), list);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testFilter,
+              (const std::vector<int> &list, uint8_t threshold)) {
+  auto f = [=](int x){return x % 255 < threshold; };
+  auto actual = parlay::filter(list, f);
+  parlay::sequence<int> expected;
+  std::copy_if(list.begin(), list.end(), std::back_inserter(expected), f);
+  RC_ASSERT(expected == actual);
+  // Filter is idempotent
+  RC_ASSERT(actual == parlay::filter(actual, f));
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMerge,
+              (std::vector<int> list1, std::vector<int> list2)) {
+  std::sort(list1.begin(), list1.end());
+  std::sort(list2.begin(), list2.end());
+  auto actual = parlay::merge(list1, list2);
+  parlay::sequence<int> expected;
+  std::merge(list1.begin(),list1.end(), list2.begin(), list2.end(),
+             std::back_inserter(expected));
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMergeCustomComparator,
+              (std::vector<int> list1, std::vector<int> list2)) {
+  auto comp = std::greater<int>();
+  std::sort(list1.begin(), list1.end(), comp);
+  std::sort(list2.begin(), list2.end(), comp);
+  auto actual = parlay::merge(list1, list2, comp);
+  parlay::sequence<int> expected;
+  std::merge(list1.begin(),list1.end(), list2.begin(), list2.end(),
+             std::back_inserter(expected), comp);
+  RC_ASSERT(expected == actual);
+}
+
 // Boolean functions
 
 RC_GTEST_PROP(TestPrimitivePropertyBased,
               testBoolFunctions,
               (const std::vector<bool> &list)) {
-  bool all_of = parlay::all_of(list, [](auto x) {return x;}),
-       none_of = parlay::none_of(list, [](auto x) {return x;}),
+  auto identity = [](auto x) {return x;};
+  bool all_of = parlay::all_of(list, identity),
+       none_of = parlay::none_of(list, identity),
        neg_none_of = parlay::none_of(list, [](auto x) {return !x;}),
-       any_of = parlay::any_of(list, [](auto x) {return x;});
+       any_of = parlay::any_of(list, identity);
   // All is true is equivalent to none is false
   RC_ASSERT(all_of == neg_none_of);
   // Either at least one is true or none is true
   RC_ASSERT(any_of ^ none_of);
+
+  RC_ASSERT(all_of == std::all_of(list.begin(), list.end(), identity));
+  RC_ASSERT(any_of == std::any_of(list.begin(), list.end(), identity));
+  RC_ASSERT(none_of == std::none_of(list.begin(), list.end(), identity));
 }
 
 // Permutations
