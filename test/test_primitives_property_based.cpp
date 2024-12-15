bd4e53127e6bfb481b30c959e1192c6756d18443
Peter Li <lihaohong6@gmail.com>
2024-12-14 19:49:51 -0500

    More tests for primitives

--- test/test_primitives_property_based.cpp
+++ test/test_primitives_property_based.cpp
@@ -1,3 +1,4 @@
+#include <regex>
 #include "gtest/gtest.h"
 #include "rapidcheck/gtest.h"
 
@@ -146,13 +147,31 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
 RC_GTEST_PROP(TestPrimitivePropertyBased,
               testMergeCustomComparator,
               (std::vector<int> list1, std::vector<int> list2)) {
-  auto comp = std::greater<int>();
+  auto comp = std::greater<>();
   std::sort(list1.begin(), list1.end(), comp);
   std::sort(list2.begin(), list2.end(), comp);
   auto actual = parlay::merge(list1, list2, comp);
   parlay::sequence<int> expected;
   std::merge(list1.begin(),list1.end(), list2.begin(), list2.end(),
              std::back_inserter(expected), comp);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testForEach,
+              (parlay::sequence<int> list)) {
+  parlay::sequence<int> seq(list.size());
+  parlay::for_each(parlay::iota(list.size()), [&](int i) {
+    seq[i] = list[i];
+  });
+  RC_ASSERT(seq == list);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testCountIf,
+              (std::vector<bool> list)) {
+  size_t expected = std::count_if(list.begin(), list.end(), [](bool b){return b;});
+  size_t actual = parlay::count_if(list, [](bool b) {return b;});
   RC_ASSERT(expected == actual);
 }
 
@@ -176,6 +195,101 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
   RC_ASSERT(none_of == std::none_of(list.begin(), list.end(), identity));
 }
 
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testFind,
+              (const std::vector<int> &original, size_t num, bool exist)) {
+  int search = exist ? (original.empty() ? 0 : original[num % original.size()]) : (int)num;
+  auto expected = std::find(original.begin(), original.end(), search);
+  auto actual = parlay::find(original, search);
+  RC_ASSERT(expected == actual);
+  // Test find if
+  auto pred = [=](int x) {return x == search;};
+  expected = std::find_if(original.begin(), original.end(), pred);
+  actual = parlay::find_if(original, pred);
+  RC_ASSERT(expected == actual);
+  // Test find if not
+  auto pred2 = [=](int x) {return std::abs(x) % 4 <= std::abs(int(num)) % 4; };
+  expected = std::find_if_not(original.begin(), original.end(), pred2);
+  actual = parlay::find_if_not(original, pred2);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testFindFirstOf,
+              (std::vector<int> list1, std::vector<int> list2)) {
+  if (list2.size() > 1000) {
+    list2.resize(1000);
+  }
+  auto expected = std::find_first_of(list1.begin(), list1.end(), list2.begin(), list2.end());
+  auto actual = parlay::find_first_of(list1, list2);
+  RC_ASSERT(expected == actual);
+
+  expected = std::find_end(list1.begin(), list1.end(), list2.begin(), list2.end());
+  // FIXME: segfaults on line 708 of primitives.h when list1 is empty/smaller than list2
+  RC_PRE(list1.size() >= list2.size());
+  actual = parlay::find_end(list1, list2);
+  RC_ASSERT(expected == actual);
+}
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMismatch,
+              (const std::vector<int> &list1, const std::vector<int> &list2)) {
+  auto expected = std::mismatch(list1.begin(), list1.end(), list2.begin(), list2.end());
+  auto actual = parlay::mismatch(list1, list2);
+  RC_ASSERT(expected == actual);
+}
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testEqual,
+              (const std::vector<int> &list1, const std::vector<int> &list2)) {
+  auto expected = std::equal(list1.begin(), list1.end(), list2.begin(), list2.end());
+  auto actual = parlay::equal(list1, list2);
+  RC_ASSERT(expected == actual);
+}
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testLexigraphicalCompare,
+              (const std::vector<int> &list1, const std::vector<int> &list2)) {
+  auto expected = std::lexicographical_compare(list1.begin(), list1.end(), list2.begin(), list2.end());
+  auto actual = parlay::lexicographical_compare(list1, list2);
+  RC_ASSERT(expected == actual);
+}
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testUnique,
+              (std::vector<int> list1)) {
+  auto actual = parlay::unique(list1);
+  auto expected = std::unique(list1.begin(), list1.end());
+  RC_ASSERT(parlay::sequence<int>(list1.begin(), expected) == actual);
+}
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMinMaxElement,
+              (std::vector<int> list1)) {
+  auto expected = std::min_element(list1.begin(), list1.end());
+  auto actual = parlay::min_element(list1);
+  RC_ASSERT(expected == actual);
+
+  expected = std::max_element(list1.begin(), list1.end());
+  actual = parlay::max_element(list1);
+  RC_ASSERT(expected == actual);
+
+  auto [expected1, expected2] = std::minmax_element(list1.begin(), list1.end());
+  auto [actual1, actual2] = parlay::minmax_element(list1);
+  if (expected1 != actual1) {
+    RC_ASSERT(*expected1 == *actual1);
+  }
+  if (expected2 != actual2) {
+    RC_ASSERT(*expected2 == *actual2);
+  }
+}
+
+
 // Permutations
 RC_GTEST_PROP(TestPrimitivePropertyBased,
               testReverse,
@@ -196,4 +310,212 @@ RC_GTEST_PROP(TestPrimitivePropertyBased,
   RC_ASSERT(seq2 == seq3);
   // Same as original
   RC_ASSERT(seq == seq2);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testRotate,
+              (std::vector<int> list1, size_t rotate)) {
+  rotate = rotate % (list1.size() + 1);
+  auto actual = parlay::rotate(list1, rotate);
+  std::rotate(list1.begin(), list1.begin() + (long)rotate, list1.end());
+  RC_ASSERT(parlay::sequence<int>(list1.begin(), list1.end()) == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testIsSorted,
+              (std::vector<int> list1)) {
+  auto expected = std::is_sorted(list1.begin(), list1.end());
+  auto actual = parlay::is_sorted(list1);
+  RC_ASSERT(expected == actual);
+
+  parlay::sort_inplace(list1);
+  RC_ASSERT(parlay::is_sorted(list1));
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testIsSortedUntil,
+              (std::vector<int> list1)) {
+  auto expected = std::is_sorted_until(list1.begin(), list1.end());
+  auto actual = parlay::is_sorted_until(list1);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testIsPartitioned,
+              (std::vector<int> list1)) {
+  auto pred = [](int x) {return x % 2 == 0;};
+  auto expected = std::is_partitioned(list1.begin(), list1.end(), pred);
+  auto actual = parlay::is_partitioned(list1, pred);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testRemove,
+              (std::vector<int> list1, int index)) {
+  for (int value : {index, list1.empty() ? -1 : list1[index % list1.size()]}) {
+    auto actual = parlay::remove(list1, value);
+    auto expected = std::remove(list1.begin(), list1.end(), value);
+    RC_ASSERT(std::equal(list1.begin(), expected, actual.begin()));
+  }
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testRemoveIf,
+              (std::vector<int> list1, int num)) {
+  const int MOD = 4;
+  num = std::abs(num) % MOD;
+  auto pred = [=](int x) {return x % MOD < num;};
+  auto actual = parlay::remove_if(list1, pred);
+  auto expected = std::remove_if(list1.begin(), list1.end(), pred);
+  RC_ASSERT(std::equal(list1.begin(), expected, actual.begin()));
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testFlattenSplitSame,
+              (parlay::sequence<int> list1, int num)) {
+  // Randomly split and then flatten. Should yield the same list.
+  int mod = std::abs(num) % 103 + 1;
+  auto pred = [=](int x) {return x % mod == 0; };
+  auto bools = parlay::map(list1, pred);
+  RC_ASSERT(parlay::flatten(parlay::split_at(list1, bools)) == list1);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testFlattenSplitSameDelayed,
+              (parlay::sequence<int> list1, int num)) {
+  auto map_function = [=](int x) {return x * num;};
+  auto expected = parlay::map(list1, map_function);
+  // Randomly split, map, and then flatten. Should be the same as mapping the original list.
+  int mod = std::abs(num) % 103 + 1;
+  auto pred = [=](int x) {return x % mod == 0; };
+  auto segments = parlay::split_at(list1, parlay::delayed_map(list1, pred));
+  auto actual = parlay::flatten(parlay::delayed_tabulate(segments.size(), [&](size_t i) {return parlay::delayed_map(segments[i], map_function); }));
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testTokens,
+              (const std::string &str)) {
+  std::regex words_regex("[^\\s]+");
+  auto words_begin = std::sregex_iterator(str.begin(), str.end(), words_regex);
+  auto words_end = std::sregex_iterator();
+  auto expected = parlay::map(parlay::sequence<std::smatch>(words_begin, words_end), [](const auto& match) {
+    return parlay::to_sequence(match.str());
+  });
+  auto actual = parlay::tokens(str);
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMapTokens,
+              (const std::string &str)) {
+  std::regex words_regex("[^\\s]+");
+  auto words_begin = std::sregex_iterator(str.begin(), str.end(), words_regex);
+  auto words_end = std::sregex_iterator();
+  auto expected = parlay::map(parlay::sequence<std::smatch>(words_begin, words_end), [](const auto& match) {
+    return match.str().length();
+  });
+  auto actual = parlay::map_tokens(str, [](auto s){return s.size();});
+  RC_ASSERT(expected == actual);
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testMapTokensVoid,
+              (const std::string &str)) {
+  const int LIMIT = 256, OFFSET = 128;
+  std::regex words_regex("[^\\s]+");
+  auto words_begin = std::sregex_iterator(str.begin(), str.end(), words_regex);
+  auto words_end = std::sregex_iterator();
+  auto characters = parlay::flatten(parlay::map(parlay::sequence<std::smatch>(words_begin, words_end), [](const auto& match) {
+    return parlay::to_sequence(match.str());
+  }));
+  auto frequencies = parlay::histogram_by_key(characters);
+  std::map<char, int> expected_counts(frequencies.begin(), frequencies.end());
+  std::array<std::atomic<int>, LIMIT> actual_counts{0};
+  parlay::map_tokens(str, [&](auto s){
+    for (char c : s) {
+      actual_counts[c + OFFSET].fetch_add(1);
+    }
+  });
+  for(int i = 0; i < LIMIT; i++) {
+    int expected = expected_counts[i - OFFSET];
+    int actual = actual_counts[i].load();
+    RC_ASSERT(expected == actual);
+  }
+}
+
+auto split_by_predicate(const parlay::sequence<int> &list, std::function<bool(int)> pred) {
+  parlay::sequence<parlay::sequence<int>> result;
+  parlay::sequence<int> current;
+  for (int num : list) {
+    current.push_back(num);
+    if (pred(num)) {
+      result.push_back(current);
+      current.clear();
+    }
+  }
+  result.push_back(current);
+  return result;
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testSplitAt,
+              (parlay::sequence<int> list1, int random)) {
+  int mod = std::abs(random) % 103 + 1;
+  auto pred = [=](int x) {return x % mod == 0; };
+  auto flags = parlay::map(list1, pred);
+  auto actual = parlay::split_at(list1, flags);
+  auto expected = split_by_predicate(list1, pred);
+  RC_ASSERT(expected == actual);
+
+  auto map_function = [=](auto lst) {return std::accumulate(lst.begin(), lst.end(), 0); };
+  auto expected2 = parlay::map(parlay::split_at(list1, flags), map_function);
+  auto actual2 = parlay::map_split_at(list1, flags, map_function);
+  RC_ASSERT(expected2 == actual2);
+}
+
+
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testRank,
+              (const std::vector<int> &original)) {
+  size_t n = original.size();
+  auto list = parlay::sort(parlay::tabulate(n, [&](size_t index) -> std::pair<int, size_t> {return {original[index], index};}));
+  auto ranks = parlay::rank(original);
+  for (size_t i = 0; i < n; i++) {
+    std::pair<int, size_t> target(original[i], i);
+    size_t expected = std::distance(list.begin(), std::lower_bound(list.begin(), list.end(), target));
+    RC_ASSERT(expected == ranks[i]);
+  }
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testRankWithIota,
+              (const std::vector<int> &v, int seed)) {
+  // n shouldn't be too large, otherwise the test will take forever
+  size_t n = v.size();
+  auto list = parlay::map(parlay::iota(n), [](auto x) {return (unsigned long)x;});
+  RC_ASSERT(list == parlay::rank(list));
+
+  parlay::random random(seed);
+  list = parlay::random_shuffle(list, random);
+  RC_ASSERT(list == parlay::rank(list));
+}
+
+RC_GTEST_PROP(TestPrimitivePropertyBased,
+              testKthSmallest,
+              (std::vector<int> list, int seed)) {
+  RC_PRE(!list.empty());
+  size_t n = list.size();
+  auto sorted = parlay::sort(list);
+  parlay::random random(seed);
+  // Randomly test 10 indices in search
+  for (size_t i = 0; i < 10; i++) {
+    auto index = random[i] % n;
+    auto expected = sorted[index];
+    auto actual = *parlay::kth_smallest(list, index);
+    RC_ASSERT(expected == actual);
+    actual = parlay::kth_smallest_copy(list, index);
+    RC_ASSERT(expected == actual);
+  }
 }
